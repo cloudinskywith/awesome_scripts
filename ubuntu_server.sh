8883a133c8cd9c08161a9592eeef92b854e0445b
廖保城 <liaobaocheng90@gmail.com>
2017-07-12 18:47:11 +0800

    Update ubuntu_server.sh

--- ubuntu_server.sh
+++ ubuntu_server.sh
@@ -14,3 +14,12 @@ vim config1.json
 }
 ##########
 ss-server -c config1.json -f pid1
+
+
+#### zsh 
+sudo apt-get install zsh git wget
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+chsh -s /bin/zsh
+
+
+#### 
